9a9059bf4c8560e3cb0e4eed14d7cb671199b59b
IyeddAmri <iyedamri21@gmail.com>
2024-01-25 14:19:54 +0100

    homePage done*

--- back-end/server/index.js
+++ back-end/server/index.js
@@ -6,7 +6,7 @@ const driftroute=require('../routes/driftroute')
 app.use(express.json());
 app.use('/api', straightroadsRoutes);
 app.use('/drift',driftroute )
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
